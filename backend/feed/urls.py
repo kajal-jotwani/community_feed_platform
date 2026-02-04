99071ee1b3e41a5870b5dd0487083f0e43bfb01d
kajal-jotwani <kajaljotwani06@gmail.com>
2026-02-04 21:05:07 +0530

    feat: add like endpoints and karma logic for posts and comments

--- backend/feed/urls.py
+++ backend/feed/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import health_check, post_list, post_comments
+from .views import health_check, post_list, post_comments, like_post, like_comment
 
 urlpatterns = [
     path("health/", health_check, name="health-check"),
     path("posts/", post_list, name="post-list"),
     path("posts/<int:post_id>/comments/", post_comments, name="post-comments"),
+    path("posts/<int:post_id>/like/", like_post, name="like-post"),
+    path("comments/<int:comment_id>/like/", like_comment, name="like-comment"),
 ]
--- backend/feed/views.py
+++ backend/feed/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db import transaction, IntegrityError
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
-from .models import Post, Comment
+from .models import Post, Comment, Like, KarmaEvent
 from .serializers import PostSerializer, CommentSerializer
 
 @api_view(["GET"])
@@ -48,3 +51,55 @@ def post_comments(request, post_id):
 
     serializer = CommentSerializer(root_comments, many=True)
     return Response(serializer.data)
+
+@api_view(["POST"])
+def like_post(request, post_id):
+    user = User.objects.first()  # assume authenticated user
+    post = get_object_or_404(Post, id=post_id)
+
+    try:
+        with transaction.atomic():
+            Like.objects.create(
+                user=user,
+                post=post
+            )
+
+            KarmaEvent.objects.create(
+                user=post.author,
+                points=5,
+                source_type="post_like"
+            )
+
+    except IntegrityError:
+        return Response(
+            {"detail": "Post already liked"},
+            status=400
+        )
+
+    return Response({"detail": "Post liked successfully"})
+
+@api_view(["POST"])
+def like_comment(request, comment_id):
+    user = User.objects.first()  # assume authenticated user
+    comment = get_object_or_404(Comment, id=comment_id)
+
+    try:
+        with transaction.atomic():
+            Like.objects.create(
+                user=user,
+                comment=comment
+            )
+
+            KarmaEvent.objects.create(
+                user=comment.author,
+                points=1,
+                source_type="comment_like"
+            )
+
+    except IntegrityError:
+        return Response(
+            {"detail": "Comment already liked"},
+            status=400
+        )
+
+    return Response({"detail": "Comment liked successfully"})
